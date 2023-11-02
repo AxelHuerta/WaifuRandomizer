8c7644c88c5417415e759b062b4d50f164e4ec90
Axel Huerta <axelhuerta.dev@gmail.com>
2023-11-02 12:45:56 -0600

    add zustand

---
+++ src/store/Store.tsx
@@ -0,0 +1,25 @@
+import { create } from "zustand";
+import { persist } from "zustand/middleware";
+
+type State = {
+  favoriteWaifus: string[];
+};
+
+type Actions = {
+  setFavoriteWaifus: (favoriteWaifus: string[]) => void;
+};
+
+export const useWaifuData = create(
+  persist<State & Actions>(
+    (set) => ({
+      favoriteWaifus: [""],
+
+      // setters
+      setFavoriteWaifus: (favoriteWaifus: string[]) =>
+        set(() => ({
+          favoriteWaifus,
+        })),
+    }),
+    { name: "waifuData" },
+  ),
+);
