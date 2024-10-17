54b366cf5c92e34270e88d56847ba7e2b46b8022
Axel Huerta <axelhuerta.dev@gmail.com>
2024-10-16 18:55:57 -0600

    update styles

--- src/App.tsx
+++ src/App.tsx
@@ -8,16 +8,23 @@ import {
   CardHeader,
 } from "./components/ui/card";
 import { Badge } from "./components/ui/badge";
+import { Button } from "./components/ui/button";
+import Link from "./components/ui/Link";
+import { Skeleton } from "./components/ui/skeleton";
 
 const URL = "https://api.waifu.im/search";
 
 function App() {
   const [waifu, setWaifu] = useState<Image | null>(null);
+  const [isLoading, setIsLoading] = useState(false);
 
   const getRandomWaifu = async () => {
+    setIsLoading(true);
     await axios.get(URL).then((res) => {
       setWaifu(res.data.images[0]);
     });
+
+    setIsLoading(false);
   };
 
   useEffect(() => {
@@ -25,45 +32,80 @@ function App() {
   }, []);
 
   return (
-    // TODO: Does these classes make sense?
     <div className="flex justify-center items-center min-h-screen">
-      <Card className="max-w-[1700px] w-[1700px]">
+      <Card className="max-w-[1700px] w-[1700px] h-[90vh] flex flex-col justify-between">
         <CardHeader>WaifuRandomizer</CardHeader>
-        <CardContent className="flex gap-8">
-          <img
-            src={waifu?.url}
-            alt="Waifu"
-            className="max-h-[70vh] rounded-xl"
-            loading="lazy"
-          />
-          <div>
+        <CardContent className="grid grid-cols-2 gap-8">
+          {/* image */}
+          {isLoading ? (
+            <Skeleton className="h-[70vh] w-[500px] rounded-xl" />
+          ) : (
+            <img
+              src={waifu?.url}
+              alt="Waifu"
+              className={`max-h-[70vh] rounded-xl ${
+                isLoading ? "animate-pulse" : ""
+              }`}
+              loading="lazy"
+            />
+          )}
+
+          <div className="h-[70vh]">
             <h2 className="text-3xl font-bold">ID: {waifu?.image_id}</h2>
             <ul>
               <li>Signature: {waifu?.signature}</li>
               <li>Extension: {waifu?.extension}</li>
-              <li>Dominant color: {waifu?.dominant_color}</li>
-              <li>Source: {waifu?.source}</li>
+              <li>
+                Dominant color:{" "}
+                <span
+                  className={`w-4 h-4 px-3 border-white border-2`}
+                  style={{ background: `${waifu?.dominant_color}` }}
+                ></span>
+                {waifu?.dominant_color}
+              </li>
+              <li>
+                Source: <Link href={waifu?.source} />
+              </li>
               <li>Uploaded at: {waifu?.uploaded_at}</li>
-              <li>Liked at: {waifu?.liked_at}</li>
-              <li>Is NSFW: {waifu?.is_nsfw ? "Yes" : "No"}</li>
+              {waifu?.liked_at && <li>Liked at: {waifu?.liked_at}</li>}
               <li>Width: {waifu?.width}</li>
               <li>Height: {waifu?.height}</li>
               <li>Byte size: {waifu?.byte_size}</li>
               <li>Favorites: {waifu?.favorites}</li>
-              <li>URL: {waifu?.url}</li>
-              <li>Preview URL: {waifu?.preview_url}</li>
+              <li>
+                URL: <Link href={waifu?.url} />
+              </li>
+              <li>
+                Preview URL: <Link href={waifu?.preview_url} />
+              </li>
             </ul>
 
-            {waifu?.artist && (
+            {waifu?.artist != undefined && (
               <>
                 <h3 className="text-2xl font-bold my-4">Artist</h3>
                 <ul>
                   <li>Artist ID: {waifu.artist.artist_id}</li>
                   <li>Name: {waifu.artist.name}</li>
-                  <li>Patreon: {waifu.artist.patreon}</li>
-                  <li>Pixiv: {waifu.artist.pixiv}</li>
-                  <li>Twitter: {waifu.artist.twitter}</li>
-                  <li>Deviant Art: {waifu.artist.deviant_art}</li>
+                  {waifu.artist.pixiv && (
+                    <li>
+                      Pixiv: <Link href={waifu.artist.pixiv} />
+                    </li>
+                  )}
+                  {waifu.artist.patreon && (
+                    <li>
+                      Patreon: <Link href={waifu.artist.patreon} />
+                    </li>
+                  )}
+                  {waifu.artist.twitter && (
+                    <li>
+                      Twitter: <Link href={waifu.artist.twitter} />
+                    </li>
+                  )}
+                  {waifu.artist.deviant_art && (
+                    <li>
+                      Deviant Art: <Link href={waifu.artist.deviant_art} />
+                    </li>
+                  )}
                 </ul>
               </>
             )}
@@ -72,13 +114,21 @@ function App() {
         <CardDescription className="mx-5">
           {waifu?.tags.map((tag) => {
             return (
-              <Badge key={tag.tag_id} className="mr-2 rounded-xl">
+              <Badge
+                key={tag.tag_id}
+                className="mr-2 rounded-xl"
+                variant="outline"
+              >
                 {tag.name}
               </Badge>
             );
           })}
         </CardDescription>
-        <CardFooter>El footer</CardFooter>
+        <CardFooter className="flex justify-end">
+          <Button className="rounded-full" onClick={getRandomWaifu}>
+            Random
+          </Button>
+        </CardFooter>
       </Card>
     </div>
   );
